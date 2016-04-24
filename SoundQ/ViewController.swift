ca046fcfad6d93a23c3897dbb27c5fa212ebfefc
nishilshah17 <nishil.shah17@gmail.com>
2016-04-24 11:00:52 -0500

    init

---
+++ SoundQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundQ
+//
+//  Created by Nishil Shah on 4/24/16.
+//  Copyright © 2016 Nishil Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
