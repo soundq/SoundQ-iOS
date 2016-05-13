92f407ae91fb22f70b1b1b684c28b372d0aad236
nishilshah17 <nishil.shah17@gmail.com>
2016-05-12 21:05:05 -0500

    basic horizontal scrolling buttons

--- SoundQ/HomeViewController.swift
+++ SoundQ/HomeViewController.swift
@@ -12,14 +12,14 @@ import ASHorizontalScrollView
 
 class HomeViewController: UIViewController {
     
-    @IBOutlet weak var horizontalScrollView: ASHorizontalScrollView!
+    @IBOutlet weak var horizontalScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.blackColor()
         setNavigationBar()
-        //setScrollView()
+        setScrollView()
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,18 +27,11 @@ class HomeViewController: UIViewController {
     }
     
     func setScrollView() {
-        horizontalScrollView.leftMarginPx = 10
-        horizontalScrollView.miniMarginPxBetweenItems = 0
-        horizontalScrollView.miniAppearPxOfLastItem = 15
-        let hsvHeight = horizontalScrollView.frame.size.height
-        horizontalScrollView.uniformItemSize = CGSizeMake(hsvHeight, hsvHeight)
-        
-        horizontalScrollView.setItemsMarginOnce()
-        for _ in 1...20 {
-            let button = UIButton(frame: CGRectZero)
-            button.backgroundColor = UIColor.orangeColor()
-            horizontalScrollView.addItem(button)
-        }
+        let scrollingView = colorButtonsView(CGSizeMake(100.0,50.0), buttonCount: 10)
+        horizontalScrollView.contentSize = scrollingView.frame.size
+        horizontalScrollView.addSubview(scrollingView)
+        horizontalScrollView.showsHorizontalScrollIndicator = true
+        horizontalScrollView.indicatorStyle = .Default
     }
     
     func setNavigationBar() {
@@ -50,5 +43,40 @@ class HomeViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor.blackColor()
         self.navigationController?.navigationBar.translucent = false
         self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+    }
+    
+    func colorButtonsView(buttonSize:CGSize, buttonCount:Int) -> UIView {
+        //creates color buttons in a UIView
+        let buttonView = UIView()
+        
+        buttonView.backgroundColor = UIColor.blackColor()
+        buttonView.frame.origin = CGPointMake(0,0)
+        
+        let padding = CGSizeMake(10, 10)
+        let buttonViewSize = CGSizeMake((buttonSize.width + padding.width) * CGFloat(buttonCount), buttonSize.height +  2.0 * padding.height)
+        buttonView.frame.size = buttonViewSize
+        
+        //add buttons to the view
+        var buttonPosition = CGPointMake(padding.width * 0.5, padding.height)
+        let buttonIncrement = buttonSize.width + padding.width
+        let hueIncrement = 1.0 / CGFloat(buttonCount)
+        var newHue = hueIncrement
+        
+        for _ in 0...(buttonCount - 1)  {
+            let button = UIButton(type: UIButtonType.Custom) as UIButton
+            button.frame.size = buttonSize
+            button.frame.origin = buttonPosition
+            buttonPosition.x = buttonPosition.x + buttonIncrement
+            button.backgroundColor = UIColor(hue: newHue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+            newHue = newHue + hueIncrement
+            button.addTarget(self, action: #selector(HomeViewController.colorButtonPressed(_:)), forControlEvents: .TouchUpInside)
+            buttonView.addSubview(button)
+        }
+        
+        return buttonView
+    }
+    
+    func colorButtonPressed(sender:UIButton){
+        horizontalScrollView.backgroundColor = sender.backgroundColor
     }
 }
